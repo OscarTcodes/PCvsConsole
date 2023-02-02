19bdada1ef51442326b594c4d1e58ed545907f4c
Oscar Tenezaca <52751515+OscarTcodes@users.noreply.github.com>
2023-02-02 14:24:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function visitXbox() {
+    window.location = 'https://www.xbox.com/en-US/consoles/xbox-series-x';
+}
+
+function visitPs() {
+    window.location = 'https://www.playstation.com/en-us/ps5/';
+}
+
+function visitPc2() {
+    window.location = 'https://www.newegg.com/p/3D5-0012-00291';
+}
+
+function visitPc1() {
+    window.location = 'https://www.newegg.com/abs-ali564/p/N82E16883360194';
+}
